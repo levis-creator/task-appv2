b06e24244fa1cf55c950a16fd0d7f9181311ebed
levis-creator <47003569+levis-creator@users.noreply.github.com>
2024-02-17 14:13:49 +0300

    v1.1.0

--- src/main/java/com/micosoft/simpletaskapp/task/TaskController.java
+++ src/main/java/com/micosoft/simpletaskapp/task/TaskController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
-
+@CrossOrigin
 @RestController
 @RequestMapping("/category/{categoryId}/task")
 @AllArgsConstructor
